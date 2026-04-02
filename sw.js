5fc1f8a2170d3670a5ea991765e5ded4bcd4dfe6
Nama_GitHub_Bos <Email_GitHub_Bos>
2026-04-02 20:45:26 +0700

    Update cache Service Worker ke v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NAMA_CACHE = 'sunnah-app-v1';
+const NAMA_CACHE = 'sunnah-app-v2'; // Kita ubah ke v2
 const FILE_CACHE = [
   './',
   './index.html',
@@ -8,9 +8,22 @@ const FILE_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+  // Langsung aktifkan service worker baru
+  self.skipWaiting();
   event.waitUntil(
     caches.open(NAMA_CACHE).then(cache => {
       return cache.addAll(FILE_CACHE);
+    })
+  );
+});
+
+// Menghapus cache versi lama saat versi baru aktif
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== NAMA_CACHE).map(key => caches.delete(key))
+      );
     })
   );
 });
