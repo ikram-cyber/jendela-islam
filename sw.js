2f2f8a5d87d2fdcc34b7085dfa35250123668857
ikram-cyber <ikram.ujrc@gmail.com>
2026-04-03 18:10:55 +0700

    Update PWA tanpa file workflow

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-const NAMA_CACHE = 'sunnah-app-v3';
-const FILE_CACHE = [ './', './index.html', './style.css', './app.js', './manifest.json' ];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(NAMA_CACHE).then(cache => cache.addAll(FILE_CACHE)));
+self.addEventListener('install', (e) => {
+    console.log('Service Worker: Terpasang');
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== NAMA_CACHE).map(key => caches.delete(key)));
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('fetch', (e) => {
+    // Biarin kosong dulu buat syarat PWA
 });
